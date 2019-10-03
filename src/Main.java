0bd149061a850a8fabadb13f9845c92bad5f2a65
Nesrin Abdulaziz Alsayed <nesrin.abdulaziz14@gmail.com>
2019-10-03 12:33:33 +0200

    Add parser
    added a parser that taken a grid, parses the grid into the format given in the description, extracting the positions of warriors and stones into data structures.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+
 public class Main {
-	public static String solve(String grid, String strategy, boolean visualize) {
-		return null;
-	}
+    static int n, m, ix, iy, tx, ty;
+    static Point[] warriors;
+    static Point[] stones;
+    static TreeMap<Point, Integer> warriorIdx;
+    static TreeMap<Point, Integer> stoneIdx;
+
+    public static String solve(String grid, String strategy, boolean visualize) {
+        return null;
+    }
+    
+    static void parser(String grid) {
+        String[] variables = grid.split(";");
+        StringTokenizer st = new StringTokenizer(variables[0] + "," + variables[1] + "," + variables[2], ",");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        ix = Integer.parseInt(st.nextToken());
+        iy = Integer.parseInt(st.nextToken());
+        tx = Integer.parseInt(st.nextToken());
+        ty = Integer.parseInt(st.nextToken());
+        stones = stonesAndWarriors(variables[3], stoneIdx = new TreeMap<>((a, b) -> (a.x != b.x ? a.x - b.x : a.y - b.y)));
+        warriors = stonesAndWarriors(variables[4], warriorIdx = new TreeMap<>((a, b) -> (a.x != b.x ? a.x - b.x : a.y - b.y)));
+    }
+
+    static Point[] stonesAndWarriors(String representation, TreeMap<Point, Integer> ptsToIdx) {
+        StringTokenizer st = new StringTokenizer(representation, ",");
+        Point[] pts = new Point[st.countTokens()];
+        for (int i = 0; i < pts.length; i++) {
+            int x = Integer.parseInt(st.nextToken()), y = Integer.parseInt(st.nextToken());
+            pts[i] = new Point(x, y);
+            ptsToIdx.put(new Point(x, y), i);
+        }
+        return pts;
+    }
+
+
 }
