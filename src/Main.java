27fe0c08382edc4a6bc94a59cafac3370f8a9011
OmarElkilany <omar.alkilani23@gmail.com>
2019-10-03 12:30:15 +0200

    fix: move  performGeneralSearch

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
 public class Main {
+	
+	// TODO: implement performGeneralSearch
+	public static Node performGeneralSearch() {
+		return new Node();
+	}
+	
 	public static String solve(String grid, String strategy, boolean visualize) {
 		return null;
 	}
