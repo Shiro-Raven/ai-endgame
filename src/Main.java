2b3660c6ca07624819871c05e065244090d3c8f3
OmarElkilany <omar.alkilani23@gmail.com>
2019-10-03 12:39:31 +0200

    fix: add genericSearchProblem parameter

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
-	
+
 	// TODO: implement performGeneralSearch
-	public static Node performGeneralSearch() {
+	public static Node performGeneralSearch(GenericSearchProblem problem) {
 		return new Node();
 	}
-	
+
 	public static String solve(String grid, String strategy, boolean visualize) {
 		return null;
 	}
