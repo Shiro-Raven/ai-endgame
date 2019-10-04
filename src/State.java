49acca36fac566a6557396bf642c113cd7d09711
Shiro-Raven <exclass9.24@gmail.com>
2019-10-04 13:11:08 +0200

    feat: added Comparable

--- src/State.java
+++ src/State.java
@@ -1,4 +1,22 @@
+import java.util.HashMap;
 
-public class State {
-
+public class State implements Comparable<State> {
+	private HashMap<String, Object> contents;
+	
+	public State(){
+		contents = new HashMap<String, Object>();
+	}
+	
+	public Object getValue(String key){
+		return contents.get(key);
+	}
+	
+	public void setValue(String key, Object value){
+		contents.put(key, value);
+	}
+	
+	public int compareTo(State arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}	
 }
